3a0fa34edd3bf6c58328876f8f3dc64b9ae0b0f9
rhysemmas <rhys.emmas@ntlworld.com>
2023-10-05 11:49:37 +0100

    Actual selection sort

--- sorting/selection.rb
+++ sorting/selection.rb
@@ -1,4 +1,30 @@
 def selection_sort(array)
+  # init a lowest value index var at the start of the array
+  lowest_value_index = 0
+  # init a starting index var at the start of the array
+  starting_index = 0
+
+  # for each index in the array (the value we will compare to)
+  (starting_index..array.length - 1).each do |i|
+    # start another loop through the array to find the lowest value
+    (starting_index..array.length - 1).each do |j|
+      # if we find a value lower than the current index, update our lowest_value_index var
+      lowest_value_index = j if array[j] < array[lowest_value_index]
+    end
+    # swap the starting point of the pass with the lowest value found in the array
+    array[i], array[lowest_value_index] = array[lowest_value_index], array[i]
+    # increment the starting point for the next pass
+    starting_index += 1
+    # if our starting point is at the end of the array, we have finished sorting
+    break if starting_index == array.length - 1
+  end
+  array
+end
+
+array = selection_sort([9, 7, 6, 8, 3, 1, 2])
+puts "got selection sorted array: #{array}"
+
+def not_quite_selection_sort(array)
   # init sorted var as false
   sorted = false
   # set the start point for a pass - nothing is sorted now so start at index 0
@@ -21,10 +47,10 @@ def selection_sort(array)
   array
 end
 
-array = selection_sort([1, 3, 2, 7, 11, 10])
+array = not_quite_selection_sort([1, 3, 2, 7, 11, 10])
 puts "got sorted array: #{array}"
 
-array = selection_sort([-9, 3, 2, 7, 11, -5])
+array = not_quite_selection_sort([-9, 3, 2, 7, 11, -5])
 puts "got sorted array: #{array}"
 
 # is this just a bubble sort that bubbles small values to the left?
