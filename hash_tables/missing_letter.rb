1b45b5171a106a413bf3a8bf8fbe4c95d3104047
rhysemmas <rhys.emmas@ntlworld.com>
2023-10-06 17:16:32 +0100

    Slightly faster

--- hash_tables/missing_letter.rb
+++ hash_tables/missing_letter.rb
@@ -23,3 +23,22 @@ puts "found missing letter: #{letter}"
 
 # this is O(n) - it is really O(26 + N + 26), but is there any way to speed it up,
 # and maybe drop the iteration of the alphabet map to see which char was missing?
+#
+# can drop the first iteration by just storing the string in the map,
+# then iterating the alphabet and checking for the missing key
+def slightly_faster_missing_letter(string)
+  hash = {}
+
+  string.each_char do |s|
+    hash[s] = true
+  end
+
+  ('a'..'z').each do |c|
+    return c unless hash.key?(c)
+  end
+
+  ''
+end
+
+letter = missing_letter('the quick brown fox jumps over a lazy bog')
+puts "found missing letter: #{letter}"
